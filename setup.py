3eafd01bc5f58a1b24111d6b0df65c9f28a9c93e
Wei Kang <weikang9009@gmail.com>
2018-08-10 12:18:25 -0700

    version pointpats only in pointpats/__ini__.py

--- setup.py
+++ setup.py
@@ -2,44 +2,65 @@ from setuptools import setup,find_packages
 import os.path
 from distutils.command.build_py import build_py
 
+# Get __version__ from PACKAGE_NAME/__init__.py without importing the package
+# __version__ has to be defined in the first line
+with open('pointpats/__init__.py', 'r') as f:
+    exec(f.readline())
+
 with open('README.rst', 'r', encoding='utf8') as file:
     long_description = file.read()
 
-pth = os.path.dirname(os.path.abspath(__file__))+ '/requirements.txt'
+def _get_requirements_from_files(groups_files):
+    groups_reqlist = {}
 
-REQUIREMENTS = [i.strip() for i in open(pth).readlines()]
+    for k,v in groups_files.items():
+        with open(v, 'r') as f:
+            pkg_list = f.read().splitlines()
+        groups_reqlist[k] = pkg_list
 
-Major = 1
-Feature = 1
-Bug = 0
-version = '%d.%d.%d' % (Major, Feature, Bug)
+    return groups_reqlist
 
-setup(name='pointpats',
-      version=version,
-      description='Methods and Functions for planar point pattern analysis',
-      long_description = long_description,
-      url='https://github.com/pysal/pointpats',
-      maintainer='Hu Shao',
-      maintainer_email='shaohutiger@gmail.com',
-      py_modules=['pointpats'],
-      python_requires='>3.4',
-      test_suite = 'nose.collector',
-      tests_require=['nose'],
-      keywords='spatial statistics',
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: GIS',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-        ],
-      license='3-Clause BSD',
-      packages=find_packages(),
-      install_requires=REQUIREMENTS,
-      zip_safe=False,
-      cmdclass = {'build.py':build_py})
+def setup_package():
+
+    _groups_files = {
+        'base': 'requirements.txt', #basic requirements
+        'tests': 'requirements_tests.txt' #requirements for tests
+        # 'docs': 'requirements_docs.txt' #requirements for building docs
+    }
+    reqs = _get_requirements_from_files(_groups_files)
+    install_reqs = reqs.pop('base')
+    extras_reqs = reqs
+
+    setup(name='pointpats',
+          version=__version__,
+          description='Methods and Functions for planar point pattern analysis',
+          long_description = long_description,
+          url='https://github.com/pysal/pointpats',
+          maintainer='Hu Shao',
+          maintainer_email='shaohutiger@gmail.com',
+          py_modules=['pointpats'],
+          python_requires='>3.4',
+          test_suite = 'nose.collector',
+          tests_require=['nose'],
+          keywords='spatial statistics',
+          classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: GIS',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+            ],
+          license='3-Clause BSD',
+          packages=find_packages(),
+          install_requires=install_reqs,
+          extras_require=extras_reqs,
+          zip_safe=False,
+          cmdclass = {'build.py':build_py})
+
+if __name__ == '__main__':
+    setup_package()
