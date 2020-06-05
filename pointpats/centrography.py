d33c47dd275fd3f4787b4249fe95b39c98ce38b8
ljwolf <levi.john.wolf@gmail.com>
2020-06-05 14:52:26 +0100

    add numba-fied skyum

--- pointpats/centrography.py
+++ pointpats/centrography.py
@@ -143,9 +143,8 @@ def weighted_mean_center(points, weights):
              (2,), (x,y) coordinates of the weighted mean center.
     """
 
-
     points, weights = np.asarray(points), np.asarray(weights)
-    w = weights * 1. / weights.sum()
+    w = weights * 1.0 / weights.sum()
     w.shape = (1, len(points))
     return np.dot(w, points)[0]
 
@@ -190,7 +189,7 @@ def std_distance(points):
     points = np.asarray(points)
     n, p = points.shape
     m = points.mean(axis=0)
-    return np.sqrt(((points*points).sum(axis=0)/n - m*m).sum())
+    return np.sqrt(((points * points).sum(axis=0) / n - m * m).sum())
 
 
 def ellipse(points):
@@ -227,14 +226,14 @@ def ellipse(points):
     xss = (xd * xd).sum()
     yss = (yd * yd).sum()
     cv = (xd * yd).sum()
-    num = (xss - yss) + np.sqrt((xss - yss)**2 + 4 * (cv)**2)
+    num = (xss - yss) + np.sqrt((xss - yss) ** 2 + 4 * (cv) ** 2)
     den = 2 * cv
     theta = np.arctan(num / den)
     cos_theta = np.cos(theta)
     sin_theta = np.sin(theta)
     n_2 = n - 2
-    sd_x = (2 * (xd * cos_theta - yd * sin_theta)**2).sum() / n_2
-    sd_y = (2 * (xd * sin_theta - yd * cos_theta)**2).sum() / n_2
+    sd_x = (2 * (xd * cos_theta - yd * sin_theta) ** 2).sum() / n_2
+    sd_y = (2 * (xd * sin_theta - yd * cos_theta) ** 2).sum() / n_2
     return np.sqrt(sd_x), np.sqrt(sd_y), theta
 
 
@@ -258,8 +257,9 @@ def dtot(coord, points):
     points = np.asarray(points)
     xd = points[:, 0] - coord[0]
     yd = points[:, 1] - coord[1]
-    d = np.sqrt(xd*xd + yd*yd).sum()
+    d = np.sqrt(xd * xd + yd * yd).sum()
     return d
+
 
 def euclidean_median(points):
     """
@@ -296,82 +296,212 @@ def minimum_bounding_circle(points):
     * If not, remove p from set.
 
     """
-    points = hull(points).tolist()
+    points = hull(points)
     if not_clockwise(points):
-        points.reverse()
+        points = points[::-1]
         if not_clockwise(points):
-            raise Exception('Points are neither clockwise nor counterclockwise')
+            raise Exception("Points are neither clockwise nor counterclockwise")
     POINTS = copy.deepcopy(points)
     removed = []
-    i=0
+    i = 0
+    if HAS_NUMBA:
+        return _skyum_numba(points)[0]
+    else:
+        return _skyum_lists(points)[0]
+
+
+def skyum(points):
+    warnings.warn(
+        "This function will be deprecated in the next release of pointpats.",
+        DeprecationWarning,
+    )
+    return minimum_bounding_circle(points)
+
+
+skyum.__doc__ = minimum_bounding_circle.__doc__
+
+
+def _skyum_lists(points):
+    points = points.tolist()
+    removed = []
+    i = 0
     while True:
-        angles = [_angle(_prec(p, points), p, _succ(p, points)) for p in points]
-        circles = [_circle(_prec(p, points), p, _succ(p, points)) for p in points]
+        angles = [_angle(_prec(p, points), p, _succ(p, points),) for p in points]
+        circles = [_circle(_prec(p, points), p, _succ(p, points),) for p in points]
         radii = [c[0] for c in circles]
-        lexord = np.lexsort((radii, angles)) #confusing as hell defaults...
+        lexord = np.lexsort((radii, angles))  # confusing as hell defaults...
         lexmax = lexord[-1]
-        candidate = (_prec(points[lexmax], points),
-                     points[lexmax],
-                     _succ(points[lexmax], points))
-        if angles[lexmax] <= PI/2.0:
-            #print("Constrained by points: {}".format(candidate))
+        candidate = (
+            _prec(points[lexmax], points),
+            points[lexmax],
+            _succ(points[lexmax], points),
+        )
+        if angles[lexmax] <= (np.pi / 2.0):
+            # print("Constrained by points: {}".format(candidate))
             return _circle(*candidate), points, removed, candidate
         else:
             try:
                 removed.append((points.pop(lexmax), i))
             except IndexError:
                 raise Exception("Construction of Minimum Bounding Circle failed!")
-        i+=1
+        i += 1
 
-def _angle(p,q,r):
-    """
-    compute the positive angle formed by PQR
-    """
-    return np.abs(get_angle_between(Ray(q,p),Ray(q,r)))
 
-def _prec(p,l):
+try:
+    from numba import njit, boolean
+
+    HAS_NUMBA = True
+
+    @njit(fastmath=True)
+    def _skyum_numba(points):
+        i = 0
+        complete = False
+        while not complete:
+            complete, points, candidate, circle = _skyum_iteration(points)
+            if complete:
+                return circle, points, None, candidate
+
+    @njit(fastmath=True)
+    def _skyum_iteration(points):
+        points = points.reshape(-1, 2)
+        n = points.shape[0]
+        angles = np.empty((n,))
+        circles = np.empty((n, 3))
+        for i in range(n):
+            p = points[(i - 1) % n]
+            q = points[i % n]
+            r = points[(i + 1) % n]
+            angles[i] = _angle(p, q, r)
+            circles[i] = _circle(p, q, r)
+        radii = circles[:, 0]
+        # workaround for no lexsort in numba
+        angle_argmax = angles.argmax()
+        angle_max = angles[angle_argmax]
+        # the maximum radius for the largest angle
+        lexmax = (radii * (angles == angle_max)).argmax()
+
+        candidate = (lexmax - 1) % n, lexmax, (lexmax + 1) % n
+        if angles[lexmax] <= (np.pi / 2.0):
+            return True, points, lexmax, circles[lexmax]
+        else:
+            mask = np.ones((n,), dtype=boolean)
+            mask[lexmax] = False
+            new_points = points[mask, :]
+            return False, new_points, lexmax, circles[lexmax]
+
+
+except ModuleNotFoundError:
+
+    def njit(func, **kwargs):
+        return func
+
+
+@njit
+def _angle(p, q, r):
+    pq = p - q
+    rq = r - q
+    magnitudes = np.linalg.norm(pq) * np.linalg.norm(rq)
+    return np.abs(np.arccos(np.dot(pq, rq) / magnitudes))
+
+
+def _prec(p, l):
     """
     retrieve the predecessor of p in list l
     """
     pos = l.index(p)
-    if pos-1 < 0:
+    if pos - 1 < 0:
         return l[-1]
     else:
-        return l[pos-1]
+        return l[pos - 1]
 
-def _succ(p,l):
+
+def _succ(p, l):
     """
     retrieve the successor of p in list l
     """
     pos = l.index(p)
-    if pos+1 >= len(l):
+    if pos + 1 >= len(l):
         return l[0]
     else:
-        return l[pos+1]
+        return l[pos + 1]
 
-def _circle(p,q,r, dmetric=dist.euclidean):
+
+@njit
+def _euclidean_distance(px, py, qx, qy):
+    return np.sqrt((px - qx) ** 2 + (py - qy) ** 2)
+
+
+@njit
+def _circle(p, q, r, dmetric=_euclidean_distance):
     """
     Returns (radius, (center_x, center_y)) of the circumscribed circle by the
     triangle pqr.
 
     note, this does not assume that p!=q!=r
     """
-    px,py = p
-    qx,qy = q
-    rx,ry = r
-    if np.allclose(np.abs(_angle(p,q,r)), PI):
-        radius = dmetric(p,r)/2.
-        center_x = (px + rx)/2.
-        center_y = (py + ry)/2.
-    elif np.allclose(np.abs(_angle(p,q,r)), 0):
-        radius = dmetric(p,q)/2.
-        center_x = (px + qx)/2.
-        center_y = (py + qy)/2.
+    px, py = p
+    qx, qy = q
+    rx, ry = r
+    angle = np.abs(_angle(p, q, r))
+    if np.abs(angle - np.pi) < 1e-5:  # angle is pi
+        radius = _euclidean_distance(px, py, rx, ry) / 2.0
+        center_x = (px + rx) / 2.0
+        center_y = (py + ry) / 2.0
+    elif np.abs(angle) < 1e-5:  # angle is zero
+        radius = _euclidean_distance(px, py, qx, qy) / 2.0
+        center_x = (px + qx) / 2.0
+        center_y = (py + qy) / 2.0
     else:
-        D = 2*(px*(qy - ry) + qx*(ry - py) + rx*(py - qy))
-        center_x = ((px**2 + py**2)*(qy-ry) + (qx**2 + qy**2)*(ry-py)
-                  + (rx**2 + ry**2)*(py-qy)) / float(D)
-        center_y = ((px**2 + py**2)*(rx-qx) + (qx**2 + qy**2)*(px-rx)
-                  + (rx**2 + ry**2)*(qx-px)) / float(D)
-        radius = dmetric((center_x, center_y), p)
-    return radius, (center_x, center_y)
+        D = 2 * (px * (qy - ry) + qx * (ry - py) + rx * (py - qy))
+        center_x = (
+            (px ** 2 + py ** 2) * (qy - ry)
+            + (qx ** 2 + qy ** 2) * (ry - py)
+            + (rx ** 2 + ry ** 2) * (py - qy)
+        ) / float(D)
+        center_y = (
+            (px ** 2 + py ** 2) * (rx - qx)
+            + (qx ** 2 + qy ** 2) * (px - rx)
+            + (rx ** 2 + ry ** 2) * (qx - px)
+        ) / float(D)
+        radius = _euclidean_distance(center_x, center_y, px, py)
+    return radius, center_x, center_y
+
+
+if __name__ == "__main__":
+    import numpy
+    from pointpats import centrography
+    from scipy import spatial
+
+    points = numpy.random.random(size=(1000, 2))
+    plist = points.tolist()
+    chull = spatial.ConvexHull(points)
+    cpoints = chull.points[chull.vertices]
+
+    # n = points.shape[0]
+    # new_angles = np.empty((n,))
+    # new_circles = np.empty((n, 3))
+    # for i in range(n):
+    #     p = points[(i - 1) % n]
+    #     q = points[i % n]
+    #     r = points[(i + 1) % n]
+    #     new_angles[i] = _angle(p, q, r)
+    #     new_circles[i] = _circle(p, q, r)
+    # old_angles = [
+    #     _angle(np.asarray(_prec(p, plist)), np.asarray(p), np.asarray(_succ(p, plist)),)
+    #     for p in plist
+    # ]
+    # old_circles = [
+    #     _circle(
+    #         np.asarray(_prec(p, plist)), np.asarray(p), np.asarray(_succ(p, plist)),
+    #     )
+    #     for p in plist
+    # ]
+    sn = centrography._skyum_numba(cpoints)
+    # sl = centrography._skyum_lists(cpoints)
+
+    import matplotlib.pyplot as plt
+
+    circ = plt.Circle(tuple(sn[0][1:]), radius=sn[0][0])
+    plt.gca().add_artist(circ)
+    plt.scatter(*points.T, color="k")
+    plt.show()
