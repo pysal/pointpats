7e8c71a29407a8f40d347d9ff22da4c3a5153c79
Martin Fleischmann <martin@martinfleischmann.net>
2023-09-26 09:40:43 +0200

    tests

--- pointpats/tests/test_kde.py
+++ pointpats/tests/test_kde.py
@@ -28,71 +28,76 @@ class TestDensity:
         )
 
     def test_default(self):
-        qm = plot_density(self.points, 10)
-        collections = list(qm.collections)
-        assert len(collections) == 12
-        for col in collections:
-            assert col.get_linewidths() == np.array(1.5)
+        ax = plot_density(self.points, 10)
+        contourset = ax.collections[0]
+        assert len(contourset.collections) == 12
+        assert not contourset.filled
+        np.testing.assert_array_equal(contourset.get_linewidths(), np.array([1.5] * 12))
         np.testing.assert_array_equal(
-            collections[5].get_edgecolor(),
-            np.array([[0.143343, 0.522773, 0.556295, 1.0]]),
+            contourset.get_edgecolor()[5],
+            np.array([0.143343, 0.522773, 0.556295, 1.0]),
         )
 
     def test_bandwidth(self):
-        qm = plot_density(self.points, 1)
-        collections = list(qm.collections)
-        assert len(collections) == 10
+        ax = plot_density(self.points, 1)
+        contourset = ax.collections[0]
+        assert len(contourset.collections) == 10
 
     def test_resolution(self):
-        qm = plot_density(self.points, 10, resolution=200)
-        collections = list(qm.collections)
+        ax = plot_density(self.points, 10, resolution=200)
+        contourset = ax.collections[0]
+        collections = contourset.collections
         assert len(collections) == 12
 
     def test_margin(self):
-        qm = plot_density(self.points, 10, margin=.3)
-        collections = list(qm.collections)
+        ax = plot_density(self.points, 10, margin=0.3)
+        contourset = ax.collections[0]
+        collections = contourset.collections
         assert len(collections) == 12
 
     def test_kdepy(self):
-        qm = plot_density(self.points, 10, kernel="gaussian")
-        collections = list(qm.collections)
+        ax = plot_density(self.points, 10, kernel="gaussian")
+        contourset = ax.collections[0]
+        collections = contourset.collections
         assert len(collections) == 12
-        for col in collections:
-            assert col.get_linewidths() == np.array(1.5)
+        np.testing.assert_array_equal(contourset.get_linewidths(), np.array([1.5] * 12))
 
     def test_levels(self):
-        qm = plot_density(self.points, 10, levels=5)
-        collections = list(qm.collections)
+        ax = plot_density(self.points, 10, levels=5)
+        contourset = ax.collections[0]
+        collections = contourset.collections
         assert len(collections) == 7
 
     def test_fill(self):
-        qm = plot_density(self.points, 10, fill=True)
-        collections = list(qm.collections)
-        assert collections[0].get_edgecolor().shape == (0, 4)
+        ax = plot_density(self.points, 10, fill=True)
+        contourset = ax.collections[0]
+        assert contourset.get_edgecolor().shape == (0, 4)
+        assert contourset.filled
         np.testing.assert_array_equal(
-            collections[0].get_facecolor(),
-            np.array([[0.279566, 0.067836, 0.391917, 1.0]]),
+            contourset.get_facecolor()[0],
+            np.array([0.279566, 0.067836, 0.391917, 1.0]),
         )
 
     def test_geopandas(self):
         geopandas = pytest.importorskip("geopandas")
 
         gs = geopandas.GeoSeries.from_xy(*self.points.T)
-        qm = plot_density(gs, 10)
-        collections = list(qm.collections)
+        ax = plot_density(gs, 10)
+        contourset = ax.collections[0]
+        collections = contourset.collections
         assert len(collections) == 12
-        for col in collections:
-            assert col.get_linewidths() == np.array(1.5)
+        np.testing.assert_array_equal(contourset.get_linewidths(), np.array([1.5] * 12))
 
     def test_kwargs(self):
-        qm = plot_density(
+        ax = plot_density(
             self.points, 10, cmap="magma", linewidths=0.5, linestyles="-."
         )
-        collections = list(qm.collections)
+        contourset = ax.collections[0]
+        collections = contourset.collections
         assert len(collections) == 12
-        for col in collections:
-            assert col.get_linewidths() == np.array(0.5)
+        np.testing.assert_array_equal(contourset.get_linewidths(), np.array([0.5] * 12))
+
         np.testing.assert_array_equal(
-            collections[5].get_edgecolor(),
-            np.array([[0.639216, 0.189921, 0.49415, 1.0]]),
+            contourset.get_edgecolor()[5],
+            np.array([0.639216, 0.189921, 0.49415, 1.0]),
         )
