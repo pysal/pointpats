BUG: fix angle of rotation in ellipse

pointpats/centrography.py
@@ -169,15 +169,15 @@ def minimum_rotated_rectangle(points, return_angle=False):
 
     >>> minimum_rotated_rectangle(coords)
     array([[107.91345156,  73.47376296],
-       [ 36.40164577, 104.61744544],
-       [  4.08727852,  30.41752523],
-       [ 75.5990843 ,  -0.72615725]])
+           [ 36.40164577, 104.61744544],
+           [  4.08727852,  30.41752523],
+           [ 75.5990843 ,  -0.72615725]])
 
     >>> minimum_rotated_rectangle(coords, return_angle=True)
     (array([[107.91345156,  73.47376296],
-            [ 36.40164577, 104.61744544],
-            [  4.08727852,  30.41752523],
-            [ 75.5990843 ,  -0.72615725]]), 66.46667861350298)
+           [ 36.40164577, 104.61744544],
+           [  4.08727852,  30.41752523],
+           [ 75.5990843 ,  -0.72615725]]), 66.46667861350298)
 
     Passing a GeoPandas object returns a shapely geometry.
 
@@ -609,7 +609,7 @@ def ellipse(points):
     -----
     Implements approach from:
 
-    https://www.icpsr.umich.edu/CrimeStat/files/CrimeStatChapter.4.pdf
+               Ebdon, D. (1985) Statistics in Geography. Basil Blackwell. Second Edition.
 
     Examples
     --------
@@ -638,13 +638,13 @@ def ellipse(points):
     semi-minor axis and clockwise rotation angle of the ellipse.
 
     >>> ellipse(coords)
-    (np.float64(37.952226702678644), np.float64(45.55366350677291), np.float64(1.2242381172906325))
+    (np.float64(24.27432576090484), np.float64(32.063444153774746), np.float64(-1.2242381172906325))
 
     Passing a GeoPandas object returns a shapely geometry.
 
     >>> geoms = gpd.GeoSeries.from_xy(*coords.T)
     >>> ellipse(geoms)
-    <POLYGON ((65.291 85.294, 69.428 83.606, 73.402 81.59, 77.173 79.265, 80.706...>
+    <POLYGON ((60.645 26.767, 57.649 25.809, 54.603 25.081, 51.536 24.589, 48.47...>
     """  # noqa: E501
     try:
         points = np.asarray(points)
@@ -665,7 +665,7 @@ def _(points: np.ndarray) -> tuple[float, float, float]:
     cv = (xd * yd).sum()
     num = (xss - yss) + np.sqrt((xss - yss) ** 2 + 4 * (cv) ** 2)
     den = 2 * cv
-    theta = np.arctan(num / den)   # tan_theta = num/den
+    theta = np.arctan(num / den)  # tan_theta = num/den
     if theta < 0:
         theta = np.pi / 2 + theta
     cos = np.cos(theta)
@@ -680,8 +680,6 @@ def _(points: np.ndarray) -> tuple[float, float, float]:
     sy /= n
     sy = np.sqrt(sy)
     return sx, sy, -theta
-
-
 
 
 @ellipse.register

pointpats/tests/test_centrography.py
@@ -7,42 +7,27 @@ from pointpats import centrography
 
 from libpysal.common import RTOL
 
-points = np.array(
-    [
-        [66.22, 32.54],
-        [22.52, 22.39],
-        [31.01, 81.21],
-        [9.47, 31.02],
-        [30.78, 60.10],
-        [75.21, 58.93],
-        [79.26, 7.68],
-        [8.23, 39.93],
-        [98.73, 77.17],
-        [89.78, 42.53],
-        [65.19, 92.08],
-        [54.46, 8.48],
-    ]
-)
-geoms =  gpd.GeoSeries.from_xy(*points.T)
+# points from Ebdon, D. (1985) Statistics for Geographers.  Second Edition
+points = np.array([(1, 2), (2, 3), (2, 2), (2, 1), (3, 4), (3, 3), (3, 1), (4, 4)])
+
+
+geoms = gpd.GeoSeries.from_xy(*points.T)
 sequence = points.tolist()
 
-dispatch_types = pytest.mark.parametrize("points", [points, geoms, sequence], ids=["ndarray", "geoseries", "list"])
+dispatch_types = pytest.mark.parametrize(
+    "points", [points, geoms, sequence], ids=["ndarray", "geoseries", "list"]
+)
+
 
 @pytest.mark.skipif(
-        shapely.geos_version < (3, 12, 0),
-        reason="Requires GEOS 3.12.0 to use correct algorithm"
+    shapely.geos_version < (3, 12, 0),
+    reason="Requires GEOS 3.12.0 to use correct algorithm",
 )
 @dispatch_types
 def test_minimum_rotated_rectangle(points):
     mrr = centrography.minimum_rotated_rectangle(points)
-    known = np.array(
-        [
-            [36.40165, 104.61744],
-            [4.087286, 30.417522],
-            [75.59908, -0.726158],
-            [107.913445, 73.47376251220703],
-        ]
-    )
+    known = np.array([[2.5, 0.5], [5.0, 3.0], [3.5, 4.5], [1.0, 2.0]])
+
     if isinstance(points, gpd.GeoSeries):
         assert shapely.Polygon(known).normalize().equals_exact(mrr.normalize(), 1e-5)
     else:
@@ -57,47 +42,40 @@ def test_minimum_rotated_rectangle(points):
                 f"\ncomputed {mrr}\nknown: {known}"
             )
 
+
 @pytest.mark.skipif(
-        shapely.geos_version < (3, 12, 0),
-        reason="Requires GEOS 3.12.0 to use correct algorithm"
+    shapely.geos_version < (3, 12, 0),
+    reason="Requires GEOS 3.12.0 to use correct algorithm",
 )
 @dispatch_types
 def test_minimum_rotated_rectangle_angle(points):
     _, angle = centrography.minimum_rotated_rectangle(points, return_angle=True)
-    np.testing.assert_allclose(angle, 66.46667861350298, RTOL)
+    np.testing.assert_allclose(angle, 45.0, RTOL)
 
 
 @dispatch_types
 def test_minimum_bounding_rectangle(points):
     res = centrography.minimum_bounding_rectangle(points)
     if isinstance(points, gpd.GeoSeries):
-        assert shapely.box(
-            8.2300000000000004,
-            7.6799999999999997,
-            98.730000000000004,
-            92.079999999999998
-            ).normalize().equals_exact(res.normalize(), 1e-5)
+        assert shapely.box(1, 1, 4, 4).normalize().equals_exact(res.normalize(), 1e-5)
     else:
         min_x, min_y, max_x, max_y = res
-        np.testing.assert_allclose(min_x, 8.2300000000000004, RTOL)
-        np.testing.assert_allclose(min_y, 7.6799999999999997, RTOL)
-        np.testing.assert_allclose(max_x, 98.730000000000004, RTOL)
-        np.testing.assert_allclose(max_y, 92.079999999999998, RTOL)
+        np.testing.assert_allclose(min_x, 1, RTOL)
+        np.testing.assert_allclose(min_y, 1, RTOL)
+        np.testing.assert_allclose(max_x, 4, RTOL)
+        np.testing.assert_allclose(max_y, 4, RTOL)
+
 
 @dispatch_types
 def test_hull(points):
     hull = centrography.hull(points)
     exp = np.array(
         [
-            [31.01, 81.21],
-            [8.23, 39.93],
-            [9.47, 31.02],
-            [22.52, 22.39],
-            [54.46, 8.48],
-            [79.26, 7.68],
-            [89.78, 42.53],
-            [98.73, 77.17],
-            [65.19, 92.08],
+            [1, 2],
+            [2, 1],
+            [3, 1],
+            [4, 4],
+            [3, 4],
         ]
     )
     if isinstance(points, gpd.GeoSeries):
@@ -105,9 +83,10 @@ def test_hull(points):
     else:
         np.testing.assert_array_equal(hull, exp)
 
+
 @dispatch_types
 def test_mean_center(points):
-    exp = np.array([52.57166667, 46.17166667])
+    exp = np.array([2.5, 2.5])
     res = centrography.mean_center(points)
     if isinstance(points, gpd.GeoSeries):
         exp = shapely.Point(exp)
@@ -115,65 +94,72 @@ def test_mean_center(points):
     else:
         np.testing.assert_array_almost_equal(res, exp)
 
+
 @dispatch_types
 def test_std_distance(points):
     std = centrography.std_distance(points)
-    np.testing.assert_allclose(std, 40.149806489086714, RTOL)
+    np.testing.assert_allclose(std, 1.4142135623730951, RTOL)
+
 
 @dispatch_types
 def test_ellipse(points):
     res = centrography.ellipse(points)
     if isinstance(points, gpd.GeoSeries):
-        assert shapely.Point(52.571666666666836, 46.17166666666682).equals_exact(res.centroid, 1e-5)
-        np.testing.assert_allclose(res.area, 5313.537950951353, RTOL)
+        assert shapely.Point(2.5, 2.5).equals_exact(res.centroid, 1e-5)
+        np.testing.assert_allclose(res.area, 2.6006886199741195, RTOL)
     else:
-        np.testing.assert_allclose(res[0], 39.623867886462982, RTOL)
-        np.testing.assert_allclose(res[1], 42.753818949026815, RTOL)
-        np.testing.assert_allclose(res[2], 1.1039268428650906, RTOL)
+        np.testing.assert_allclose(res[0], 0.6640655130520275, RTOL)
+        np.testing.assert_allclose(res[1], 1.2486060204784164, RTOL)
+        np.testing.assert_allclose(res[2], -0.5535743588970453, RTOL)
+
 
 @dispatch_types
 def test_euclidean_median(points):
     euclidean = centrography.euclidean_median(points)
-    res = np.array([54.16770671, 44.4242589])
+    res = np.array([2.34998979, 2.48670953])
     if isinstance(points, gpd.GeoSeries):
         assert isinstance(euclidean, shapely.Point)
         euclidean = shapely.get_coordinates(euclidean).flatten()
     np.testing.assert_array_almost_equal(euclidean, res, decimal=3)
 
+
 @dispatch_types
 def test_minimum_bounding_circle(points):
     res = centrography.minimum_bounding_circle(points)
-    x = 55.244520477497474
-    y = 51.88135107645883
-    r = 50.304102155590726
+    x = 2.642857142857143
+    y = 2.7857142857142856
+    r = 1.821078397711709
     if isinstance(points, gpd.GeoSeries):
         assert shapely.Point(x, y).equals_exact(res.centroid, 1e-5)
-        np.testing.assert_allclose(np.sqrt(res.area /np.pi), r, 0.1)
+        np.testing.assert_allclose(np.sqrt(res.area / np.pi), r, 0.1)
     else:
         np.testing.assert_allclose(res[0][0], x, RTOL)
         np.testing.assert_allclose(res[0][1], y, RTOL)
         np.testing.assert_allclose(res[1], r, RTOL)
 
+
 @dispatch_types
 def test_weighted_mean_center(points):
-    res = centrography.weighted_mean_center(points, np.tile(np.array([1, 2, 3]), 4))
-    exp = np.array([53.18333333333333, 50.83916666666667])
+    res = centrography.weighted_mean_center(points, np.tile(np.array([1, 2]), 4))
+    exp = np.array([2.5833333, 2.5833333])
     if isinstance(points, gpd.GeoSeries):
         exp = shapely.Point(exp)
         assert exp.equals_exact(res, 1e-5)
     else:
         np.testing.assert_array_almost_equal(res, exp)
 
+
 @dispatch_types
 def test_manhattan_median(points):
     with pytest.warns(UserWarning, match="Manhattan Median is not unique for even"):
         res = centrography.manhattan_median(points)
-    exp = np.array([59.825, 41.230])
+    exp = np.array([2.5, 2.5])
     if isinstance(points, gpd.GeoSeries):
         exp = shapely.Point(exp)
         assert exp.equals_exact(res, 1e-5)
     else:
         np.testing.assert_array_almost_equal(res, exp)
+
 
 @dispatch_types
 def test_dtot(points):
@@ -181,4 +167,12 @@ def test_dtot(points):
     if isinstance(points, gpd.GeoSeries):
         coord = shapely.Point(*coord)
     res = centrography.dtot(coord, points)
-    np.testing.assert_allclose(res, 570.3801950846755, RTOL)
+    np.testing.assert_allclose(res, 260.8212494704881, RTOL)
+
+
+def test_ellipse_2():
+    p2 = np.array([[3, 4], [3, 9], [4, 7], [5, 5], [6, 3], [7, 1], [7, 6]])
+    sx, sy, theta = centrography.ellipse(p2)
+    np.testing.assert_allclose(sx, 2.6726124191242437, RTOL)
+    np.testing.assert_allclose(sy, 1.1952286093343936, RTOL)
+    np.testing.assert_allclose(theta, -1.1071487177940904)