fcc4d12059f6cbbdc7cbc57a88fde5736130036d
Serge Rey <sjsrey@gmail.com>
2026-01-05 16:21:47 -0800

    Refactor for rng and random in quadrat test

--- pointpats/process.py
+++ pointpats/process.py
@@ -142,7 +142,9 @@ class PointProcess(object):
         n = parameter["n"]
         while c < n:
             pnts = self.realize(n)
-            pnts = [ps.cg.shapes.Point((x, y)) for x, y in pnts]
+            pnts = list(pnts)
+            print(f"{type(pnts[0])=}")
+            pnts = [ps.cg.shapes.Point((x[0], y[0])) for x, y in pnts]
             pins = self.window.filter_contained(pnts)
             sample.extend(pins)
             c = len(sample)
